6b4ef0346c7aa0cff8a2b6a10890fdf659e56698
Tanim-Bin-Aziz <ahmedxaziz@gmail.com>
2023-11-02 00:47:33 +0600

    featured item added

---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import featuredImg from '../../../assets/home/featured.jpg'
+import './Featured.css';
+
+const Featured = () => {
+    return (
+        <div className='featured-item text-white pt-8 my-20'>
+            <SectionTitle
+                subheading="Check it out"
+                heading="Featured Item"
+            ></SectionTitle>
+            <div className='md:flex justify-center items-center pb-20 pt-12 px-36'>
+                <div>
+                    <img src={featuredImg} alt="" />
+                </div>
+                <div className='md:ml-10'>
+                    <p>Aug 20 2023</p>
+                    <p className='uppercase'>
+                        where i can get some?
+                    </p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit earum fuga beatae reprehenderit excepturi, rem animi exercitationem, vitae blanditiis voluptatibus corrupti iusto fugit cum harum dolorem, sit deserunt quia dolore!</p>
+                    <button className="btn btn-outline btn-secondary mt-2">Order Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Featured;
--- src/pages/Shared/MenuItem.jsx
+++ src/pages/Shared/MenuItem.jsx
@@ -5,7 +5,8 @@ const MenuItem = ({ item }) => {
     const { name, image, price, recipe } = item;
 
     return (
-        <div className="flex space-x-4">
+        <div className="flex space-x-2">
+
             <img style={{ borderRadius: '0 200px 200px 200px' }} className="w-[100px]" src={image} alt="" />
             <div>
                 <h3 className="uppercase">
