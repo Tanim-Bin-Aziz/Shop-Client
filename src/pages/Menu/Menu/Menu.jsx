ebf9c06501b0bf540b63f22ff7fb442fc16d26c7
Tanim-Bin-Aziz <ahmedxaziz@gmail.com>
2023-11-06 11:17:00 +0600

    new menu page added

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+
 
 const Menu = () => {
     return (
@@ -6,7 +9,10 @@ const Menu = () => {
             <Helmet>
                 <title>Shop | Menu</title>
             </Helmet>
+            <Cover img={menuImg} title="our menu"></Cover>
+
         </div>
+
     );
 };
 
