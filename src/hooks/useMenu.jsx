b1ceb458ab4f88a6037f611087bb8f69d7814ce6
Tanim-Bin-Aziz <ahmedxaziz@gmail.com>
2023-11-06 11:37:15 +0600

    custom hook added

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import { useEffect, useState } from "react";
 
 const useMenu = () => {
-    return (
-        <div>
+    const [menu, setMenu] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        fetch('menu.json')
+            .then(res => res.json())
+            .then(data => {
 
-        </div>
-    );
-};
-
+                setMenu(data)
+                setLoading(false);
+            });
+    }, [])
+    return [menu, loading]
+}
 export default useMenu;
